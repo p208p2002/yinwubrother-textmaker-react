4618a9f07b81764671821f08e23c91ceb26833ce
unknown <p208p2002@gmail.com>
2019-06-17 10:59:24 +0800

    share links

--- src/App.js
+++ src/App.js
@@ -2,13 +2,57 @@ import React from 'react';
 import './App.css';
 import ImgBlocks from './modules/ImgBlocks/index.jsx'
 import DisqusBlock from './modules/Disqus/index.jsx'
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  LineShareButton,
+  LineIcon,
+} from 'react-share';
 // import AdSense from 'react-adsense';
 
+var SHARE_URL = 'https://p208p2002.github.io/yinwubrother-textmaker-react/'
 class App extends React.Component {
   render() {
     return (
       <div id="App">
-        <h1 className="text-center">鸚鵡兄弟文字圖產生器</h1>
+        <div className="text-center">
+          <h1>鸚鵡兄弟文字圖產生器</h1>
+          <div className="icon-align">
+          <FacebookShareButton
+            url={SHARE_URL}
+          >
+          <FacebookIcon
+              size={32}
+              round />
+          </FacebookShareButton>
+          <LineShareButton
+            url={SHARE_URL}
+          >
+          <LineIcon
+              size={32}
+              round />
+          </LineShareButton>
+          <TwitterShareButton
+            url={SHARE_URL}
+          >
+          <TwitterIcon
+              size={32}
+              round />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            url={SHARE_URL}
+          >
+          <WhatsappIcon
+              size={32}
+              round />
+          </WhatsappShareButton>
+          </div>
+        </div>
+        <div className="text-center"><small>share links</small></div>
         <hr/>
         <div className="container">
           <ImgBlocks/>
