cfaa19a9b26fd175ae463c9691c3494251386ac9
unknown <p208p2002@gmail.com>
2019-06-18 11:44:43 +0800

    no message

---
+++ src/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "react-app",
+    "rules" : {
+        // "indent": ["error", 4] // A custom style-related rule for example
+        // More custom rules here
+        "no-undef":"off"
+    }
+}
