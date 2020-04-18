5713c1dd5f901b568d29b27e3c8ca381103ac5d0
p208p2002 <p208p2002@users.noreply.github.com>
2020-04-18 14:10:45 +0000

    deploy: 68e9780606b912e34800e0114b319481e60c5d75

---
+++ precache-manifest.4bf5f54b3d54f3cb89038081392811cb.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1923502e5dd156032b10700626d13c",
+    "url": "/yinwubrother-textmaker-react/index.html"
+  },
+  {
+    "revision": "c8f67dbf07df67be091d",
+    "url": "/yinwubrother-textmaker-react/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "222d8c9dcffde44f0f54",
+    "url": "/yinwubrother-textmaker-react/static/css/main.ef7e919e.chunk.css"
+  },
+  {
+    "revision": "c8f67dbf07df67be091d",
+    "url": "/yinwubrother-textmaker-react/static/js/2.5856cc3f.chunk.js"
+  },
+  {
+    "revision": "222d8c9dcffde44f0f54",
+    "url": "/yinwubrother-textmaker-react/static/js/main.8313b63c.chunk.js"
+  },
+  {
+    "revision": "1f1dbde0789c6276066b",
+    "url": "/yinwubrother-textmaker-react/static/js/runtime~main.269db08f.js"
+  },
+  {
+    "revision": "6ed9e07eca6d48df22ed9c6de43f942b",
+    "url": "/yinwubrother-textmaker-react/static/media/0.6ed9e07e.jpg"
+  },
+  {
+    "revision": "8cd75c649c91123a3ee43d9d9824ac6c",
+    "url": "/yinwubrother-textmaker-react/static/media/0.8cd75c64.gif"
+  },
+  {
+    "revision": "497a5c2350db1e874adf3b94e0d70188",
+    "url": "/yinwubrother-textmaker-react/static/media/1.497a5c23.jpg"
+  },
+  {
+    "revision": "5cae2bbcb0144f904f9d230a84688db1",
+    "url": "/yinwubrother-textmaker-react/static/media/1.5cae2bbc.gif"
+  },
+  {
+    "revision": "a83750471030238355702772937ba915",
+    "url": "/yinwubrother-textmaker-react/static/media/10.a8375047.gif"
+  },
+  {
+    "revision": "ddc83e71d9eb6432036cf7d534f65e98",
+    "url": "/yinwubrother-textmaker-react/static/media/10.ddc83e71.jpg"
+  },
+  {
+    "revision": "4fdf8f414b393e2cc9497bb13386faa6",
+    "url": "/yinwubrother-textmaker-react/static/media/11.4fdf8f41.gif"
+  },
+  {
+    "revision": "c3b9ed53d9eb05038c392cb750e0a58d",
+    "url": "/yinwubrother-textmaker-react/static/media/11.c3b9ed53.jpg"
+  },
+  {
+    "revision": "fa6d2eea12af1f60b05fa0d0e41beedf",
+    "url": "/yinwubrother-textmaker-react/static/media/12.fa6d2eea.jpg"
+  },
+  {
+    "revision": "9c1f842c54b1b798f672b146af7db826",
+    "url": "/yinwubrother-textmaker-react/static/media/13.9c1f842c.jpg"
+  },
+  {
+    "revision": "816c842432a4b1041d64b44c4b794b06",
+    "url": "/yinwubrother-textmaker-react/static/media/14.816c8424.jpg"
+  },
+  {
+    "revision": "6a98d44cdd2b52048fece16f6cef0e8f",
+    "url": "/yinwubrother-textmaker-react/static/media/15.6a98d44c.jpg"
+  },
+  {
+    "revision": "6cad1508f12a92175ce7a42185d416b0",
+    "url": "/yinwubrother-textmaker-react/static/media/16.6cad1508.jpg"
+  },
+  {
+    "revision": "24e9db035af5453164677bf55768142c",
+    "url": "/yinwubrother-textmaker-react/static/media/17.24e9db03.jpg"
+  },
+  {
+    "revision": "52b9ecd263cb1aa14625c7b7de0a760f",
+    "url": "/yinwubrother-textmaker-react/static/media/18.52b9ecd2.jpg"
+  },
+  {
+    "revision": "4632f04cdfa99138a7bd0d61c7951cfd",
+    "url": "/yinwubrother-textmaker-react/static/media/19.4632f04c.jpg"
+  },
+  {
+    "revision": "34d76db829a5402d458aa5b0ed5cc923",
+    "url": "/yinwubrother-textmaker-react/static/media/2.34d76db8.jpg"
+  },
+  {
+    "revision": "e4c567147b225712e1446e653201749d",
+    "url": "/yinwubrother-textmaker-react/static/media/2.e4c56714.gif"
+  },
+  {
+    "revision": "e7a960ad466b926f6979b37400638faf",
+    "url": "/yinwubrother-textmaker-react/static/media/20.e7a960ad.jpg"
+  },
+  {
+    "revision": "76a2f6f66e5b93c2011037a034b94c83",
+    "url": "/yinwubrother-textmaker-react/static/media/21.76a2f6f6.jpg"
+  },
+  {
+    "revision": "11c31507c9284f902ee6d635606fc280",
+    "url": "/yinwubrother-textmaker-react/static/media/22.11c31507.jpg"
+  },
+  {
+    "revision": "46aabc80df52d8b94ee1f45c29856be7",
+    "url": "/yinwubrother-textmaker-react/static/media/23.46aabc80.jpg"
+  },
+  {
+    "revision": "c23c243200d0589b477cef2d6b2e6012",
+    "url": "/yinwubrother-textmaker-react/static/media/24.c23c2432.jpg"
+  },
+  {
+    "revision": "9892dccf9152fafd95d30a5762835b61",
+    "url": "/yinwubrother-textmaker-react/static/media/25.9892dccf.jpg"
+  },
+  {
+    "revision": "5595f01ec7bbdbe1e9b908bcd723f7c6",
+    "url": "/yinwubrother-textmaker-react/static/media/26.5595f01e.jpg"
+  },
+  {
+    "revision": "89dfaba183d044de7b4a8b8994d42553",
+    "url": "/yinwubrother-textmaker-react/static/media/27.89dfaba1.jpg"
+  },
+  {
+    "revision": "e9914d4dcc9c38589dcd17b20e732c52",
+    "url": "/yinwubrother-textmaker-react/static/media/28.e9914d4d.jpg"
+  },
+  {
+    "revision": "4d6d92bdf42894fbe867d155c05a31cc",
+    "url": "/yinwubrother-textmaker-react/static/media/29.4d6d92bd.jpg"
+  },
+  {
+    "revision": "5a14ec93286616d3b7ed258fd815d6f7",
+    "url": "/yinwubrother-textmaker-react/static/media/3.5a14ec93.jpg"
+  },
+  {
+    "revision": "d7c35df94999a589d94db4a73a02a54d",
+    "url": "/yinwubrother-textmaker-react/static/media/3.d7c35df9.gif"
+  },
+  {
+    "revision": "0eeb0022aed552fea197608bb09c3e7c",
+    "url": "/yinwubrother-textmaker-react/static/media/30.0eeb0022.jpg"
+  },
+  {
+    "revision": "e23695b288bf0707db7c3a7085bebef6",
+    "url": "/yinwubrother-textmaker-react/static/media/31.e23695b2.jpg"
+  },
+  {
+    "revision": "7b7c63607c52997753e1c3720abe736b",
+    "url": "/yinwubrother-textmaker-react/static/media/32.7b7c6360.jpg"
+  },
+  {
+    "revision": "95fd999ff64a30b49c63f89bad4bf99b",
+    "url": "/yinwubrother-textmaker-react/static/media/33.95fd999f.jpg"
+  },
+  {
+    "revision": "a29c3732b6eefeebfcf5922224d712c4",
+    "url": "/yinwubrother-textmaker-react/static/media/34.a29c3732.jpg"
+  },
+  {
+    "revision": "5238db03786c9b8d6b7e26b712e27038",
+    "url": "/yinwubrother-textmaker-react/static/media/35.5238db03.jpg"
+  },
+  {
+    "revision": "eb4453bb575ce59ad6a73c0d348c4009",
+    "url": "/yinwubrother-textmaker-react/static/media/36.eb4453bb.jpg"
+  },
+  {
+    "revision": "5f9d7f23a9ede3cb6a947a9abdb99e0d",
+    "url": "/yinwubrother-textmaker-react/static/media/37.5f9d7f23.jpg"
+  },
+  {
+    "revision": "0ba01b93fa99f6f6cb1bf431fb7a4679",
+    "url": "/yinwubrother-textmaker-react/static/media/38.0ba01b93.jpg"
+  },
+  {
+    "revision": "21a1a7930892a6810846e421b869d47a",
+    "url": "/yinwubrother-textmaker-react/static/media/39.21a1a793.jpg"
+  },
+  {
+    "revision": "5aadbf56a31416e75eeedbf1610c909f",
+    "url": "/yinwubrother-textmaker-react/static/media/4.5aadbf56.jpg"
+  },
+  {
+    "revision": "fe1f03e32a69d1af7bdb9f06b7261f79",
+    "url": "/yinwubrother-textmaker-react/static/media/4.fe1f03e3.gif"
+  },
+  {
+    "revision": "c53a8ee5a2156a6f08a7269ce3964179",
+    "url": "/yinwubrother-textmaker-react/static/media/40.c53a8ee5.jpg"
+  },
+  {
+    "revision": "a58e522020ef2cb0f1f2c02c288b96dd",
+    "url": "/yinwubrother-textmaker-react/static/media/41.a58e5220.jpg"
+  },
+  {
+    "revision": "7c32135f60cd96f6479e0213847af7cb",
+    "url": "/yinwubrother-textmaker-react/static/media/42.7c32135f.jpg"
+  },
+  {
+    "revision": "5b93f32798902f70bda76117a5002566",
+    "url": "/yinwubrother-textmaker-react/static/media/43.5b93f327.jpg"
+  },
+  {
+    "revision": "98286a1349657d97dd2cf6a8a606d1d2",
+    "url": "/yinwubrother-textmaker-react/static/media/44.98286a13.jpg"
+  },
+  {
+    "revision": "e11119e7766d65cdcdb878aa74a8875b",
+    "url": "/yinwubrother-textmaker-react/static/media/48.e11119e7.jpg"
+  },
+  {
+    "revision": "58ed5959db53093f54615e20007f570d",
+    "url": "/yinwubrother-textmaker-react/static/media/49.58ed5959.jpg"
+  },
+  {
+    "revision": "524df7a19c866ef5ec03bfb5acbf06fc",
+    "url": "/yinwubrother-textmaker-react/static/media/5.524df7a1.gif"
+  },
+  {
+    "revision": "c74dfa7675898a9954435723192c08ee",
+    "url": "/yinwubrother-textmaker-react/static/media/5.c74dfa76.jpg"
+  },
+  {
+    "revision": "352ce7c51e6889938bd28c710f66efae",
+    "url": "/yinwubrother-textmaker-react/static/media/51.352ce7c5.jpg"
+  },
+  {
+    "revision": "d353c6b8b8bac9527467daba6d1b520e",
+    "url": "/yinwubrother-textmaker-react/static/media/52.d353c6b8.jpg"
+  },
+  {
+    "revision": "a996c79a588c091c52ca015848cfb2ce",
+    "url": "/yinwubrother-textmaker-react/static/media/54.a996c79a.jpg"
+  },
+  {
+    "revision": "6ebfa439df86b52ae4e81c2d855984cf",
+    "url": "/yinwubrother-textmaker-react/static/media/55.6ebfa439.jpg"
+  },
+  {
+    "revision": "ca1b2922cc6c95d7b757dd71c8b142ad",
+    "url": "/yinwubrother-textmaker-react/static/media/56.ca1b2922.jpg"
+  },
+  {
+    "revision": "ac55bd0547ae689eb867295832a9168d",
+    "url": "/yinwubrother-textmaker-react/static/media/58.ac55bd05.jpg"
+  },
+  {
+    "revision": "cb55b193d8642b30ba869212bd77c059",
+    "url": "/yinwubrother-textmaker-react/static/media/6.cb55b193.gif"
+  },
+  {
+    "revision": "e50c3aa0eeb1d0d2399bc68e2aacc1bb",
+    "url": "/yinwubrother-textmaker-react/static/media/6.e50c3aa0.jpg"
+  },
+  {
+    "revision": "8655651fef59e419f708a750ae3e6e8c",
+    "url": "/yinwubrother-textmaker-react/static/media/60.8655651f.jpg"
+  },
+  {
+    "revision": "dbb50654c16675dcdc38f67739b37a6a",
+    "url": "/yinwubrother-textmaker-react/static/media/61.dbb50654.jpg"
+  },
+  {
+    "revision": "529b8997b36eda16d12d5c2dc21864cf",
+    "url": "/yinwubrother-textmaker-react/static/media/63.529b8997.jpg"
+  },
+  {
+    "revision": "86857f753b3265fdf7e76614c252f428",
+    "url": "/yinwubrother-textmaker-react/static/media/7.86857f75.jpg"
+  },
+  {
+    "revision": "e62aba17da1c63127427ed82ef0e6f41",
+    "url": "/yinwubrother-textmaker-react/static/media/7.e62aba17.gif"
+  },
+  {
+    "revision": "5ee878594b67fd4d43bec2e8310adda9",
+    "url": "/yinwubrother-textmaker-react/static/media/8.5ee87859.jpg"
+  },
+  {
+    "revision": "9ea553377e3944c5fe647689004f58c2",
+    "url": "/yinwubrother-textmaker-react/static/media/8.9ea55337.gif"
+  },
+  {
+    "revision": "7a5721d5c4f810d5644f99fd50f1fd76",
+    "url": "/yinwubrother-textmaker-react/static/media/9.7a5721d5.jpg"
+  },
+  {
+    "revision": "e0eb0032d60283edaa715f0ceffa270f",
+    "url": "/yinwubrother-textmaker-react/static/media/9.e0eb0032.gif"
+  }
+]);
